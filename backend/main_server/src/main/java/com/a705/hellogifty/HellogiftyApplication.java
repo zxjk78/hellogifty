1c2c74a21fbd9e19bbf0f55231c28f259f27d975
minji <minji13579@naver.com>
2022-10-25 17:39:23 +0900

    initial commit

---
+++ backend/main_server/src/main/java/com/a705/hellogifty/HellogiftyApplication.java
@@ -0,0 +1,13 @@
+package com.a705.hellogifty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellogiftyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellogiftyApplication.class, args);
+	}
+
+}
