1ff1f37d943664ef7c079f5cd3630dab5bdc8f6d
noh yongrae <zxjk78@gmail.com>
2022-10-28 10:53:29 +0900

    Feat: Upload MMS Image dummycode wip

---
+++ frontend/src/components/readmms/ReadMMSStatusBar.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { requestReadMMSPermission } from '../../utils/getPermission';
+import { GlobalStyles } from '../../constants/style';
+import { getAllMMSAfterAccess } from '../../utils/mmsFunc';
+import { dummySendMMSImage } from '../../api/mms';
+const gColor = GlobalStyles.colors;
+
+const ReadMMSStatusBar = () => {
+  const [isReading, setIsReading] = useState(false);
+  const n = 0;
+  const [byteArr, setByteArr] = useState(null);
+  useEffect(() => {
+    requestReadMMSPermission();
+    setIsReading(true);
+
+    (async () => {
+      const tmp = await getAllMMSAfterAccess(0, (imgArr) => {
+        console.log(imgArr.length);
+        setByteArr(imgArr);
+      });
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (byteArr === null) return;
+    (async () => {
+      const imageResult = await dummySendMMSImage(byteArr);
+      console.log(imageResult);
+    })();
+    setIsReading(false);
+  }, [byteArr]);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.container}>
+        <Text>
+          {isReading
+            ? '메세지함에서 기프티콘을 찾고 있어요'
+            : `총 ${0}건의 기프티콘을 찾았어요! 등록하러 가기`}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: gColor.backgroundComponent,
+    // backgroundColor: 'red',
+    height: '35%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default ReadMMSStatusBar;
