8768c55f7fb79decff289474fd8a016dc659bd74
minji <minji13579@naver.com>
2022-11-15 17:39:01 +0900

    Feat : add myTradeGifticon api

--- backend/main_server/src/main/java/com/a705/hellogifty/api/repository/TradePostRepository.java
+++ backend/main_server/src/main/java/com/a705/hellogifty/api/repository/TradePostRepository.java
@@ -3,6 +3,7 @@ package com.a705.hellogifty.api.repository;
 import com.a705.hellogifty.api.domain.entity.LargeCategory;
 import com.a705.hellogifty.api.domain.entity.SmallCategory;
 import com.a705.hellogifty.api.domain.entity.TradePost;
+import com.a705.hellogifty.api.domain.entity.User;
 import com.a705.hellogifty.api.dto.trade_post.TradePostListResponseDto;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -23,5 +24,8 @@ public interface TradePostRepository extends JpaRepository<TradePost, Long> {
     Optional<List<TradePost>> findByTitleContainsAndGifticon_SmallCategory(String title, SmallCategory smallCategory);
 
     Optional<List<TradePost>> findByTitleContainsAndGifticon_SmallCategory_LargeCategory(String title, LargeCategory largeCategory);
+
     Optional<TradePost> findById(Long id);
+
+    Optional<List<TradePost>> findByUser(User user);
 }
