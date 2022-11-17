8a7ff306d01656b99b9c430c2666ea5082f7be47
minji <minji13579@naver.com>
2022-11-17 14:29:39 +0900

    Fix : change updateTradeState to scheduler

--- backend/main_server/src/main/java/com/a705/hellogifty/api/controller/TradeController.java
+++ backend/main_server/src/main/java/com/a705/hellogifty/api/controller/TradeController.java
@@ -95,10 +95,10 @@ public class TradeController {
         return responseService.getSuccessResult();
     }
 
-    @ApiOperation(value = "유통기한이 지난 판매글 상태 변경")
-    @PutMapping("/expiration")
-    public CommonResult expirationUpdate() {
-        tradeService.updateTradeState();
-        return responseService.getSuccessResult();
-    }
+//    @ApiOperation(value = "유통기한이 지난 판매글 상태 변경")
+//    @PutMapping("/expiration")
+//    public CommonResult expirationUpdate() {
+//        tradeService.updateTradeState();
+//        return responseService.getSuccessResult();
+//    }
 }
